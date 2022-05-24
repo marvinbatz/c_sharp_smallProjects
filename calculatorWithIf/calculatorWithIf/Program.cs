fd5cca68f8222b92c05108111da7937236cdb26f
marvinbatz <marvinbatzz@gmail.com>
2022-05-23 22:49:58 -0600

    Agrengando sentencias if y calculadora con if en c#

---
+++ calculatorWithIf/calculatorWithIf/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Ingrese 1 para sumar, 2 para restar, 3 para multiplicar y 4 para dividr.");
+int option = int.Parse(Console.ReadLine());
+float resultado = 0;
+
+if (option == 1)
+{
+    Console.WriteLine("Ingrese primer número: ");
+    float num1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese primer número: ");
+    float num2 = float.Parse(Console.ReadLine());
+    resultado = num1 + num2;
+}
+else if (option == 2)
+{
+    Console.WriteLine("Ingrese primer número: ");
+    float num1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese primer número: ");
+    float num2 = float.Parse(Console.ReadLine());
+    resultado = num1 - num2;
+}
+else if (option == 3)
+{
+    Console.WriteLine("Ingrese primer número: ");
+    float num1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese primer número: ");
+    float num2 = float.Parse(Console.ReadLine());
+    resultado = num1 * num2;
+}
+else if (option == 4)
+{
+    Console.WriteLine("Ingrese primer número: ");
+    float num1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese primer número: ");
+    float num2 = float.Parse(Console.ReadLine());
+    resultado = num1 / num2;
+}
+else
+{
+    Console.WriteLine("Esa opcion no se encuentra");
+}
+
+Console.WriteLine("El resultado es: " + resultado);
---
+++ ifStatment/ifStatment/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = 2;
+string message = "";
+
+if (num1 == 7)
+{
+    message = "My value is 7";
+}
+else if(num1 == 7 * 2)
+{
+    message = "Yeah! the number is 14";
+}
+else
+{
+    message = "My value not is 7";
+}
+
+Console.WriteLine(message);
