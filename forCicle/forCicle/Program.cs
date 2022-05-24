fd3550dbaaae948959db80c76f672c2bb43185f5
marvinbatz <marvinbatzz@gmail.com>
2022-05-23 23:53:24 -0600

    Agregando ciclos for, while y doWhile

---
+++ forCicle/forCicle/Program.cs
@@ -0,0 +1,4 @@
+﻿for (int i = 0; /* En donde inicia el contador */ i < 100; /* Mientras cumpla con esta condicion */ i = i+ 10  /* Que hara la varialbe */)
+{
+    Console.Write($"Estamos en el valor: {i} \n");
+}
---
+++ whileDoWhile/whileDoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿bool continueVerify = false;
+
+do
+{
+    Console.Write("Do you wish the software running? 1 = YES, 2 = NOT \n");
+    int option = int.Parse(Console.ReadLine());
+    if (option == 1)
+    {
+        Console.Write("Program Continue! \n");
+        continueVerify = true;
+    }
+    else if (option == 2)
+    {
+        Console.Write("Bye Bye");
+        continueVerify = false;
+    }
+    else
+    {
+        Console.Write("ERROR -- Try again \n");
+    }
+} while (continueVerify == true);
